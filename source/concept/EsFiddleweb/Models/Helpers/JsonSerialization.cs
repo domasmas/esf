6951947635872ed42a812db8ce3edc4eee627618
domasmas <domas.masiulis@gmail.com>
2016-05-22 21:05:09 +0100

    Concept source code

---
+++ source/concept/EsFiddleweb/Models/Helpers/JsonSerialization.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace EsFiddleweb.Models.Helpers
+{
+    public static class JsonSerialization
+    {
+        public static T Deserialize<T>(string text)
+        {
+            return JsonConvert.DeserializeObject<T>(text, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+        }
+
+        public static string Serialize<T>(T @object)
+        {
+            return JsonConvert.SerializeObject(@object, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+        }
+    }
+}
