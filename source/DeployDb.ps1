48274c2af6b7ac13418747bad450e6a0589e55ee
vladotrifonov@gmail.com <vladotrifonov@gmail.com>
2016-06-15 16:11:32 +0100

    Refactor deployment script into function

--- source/DeployDb.ps1
+++ source/DeployDb.ps1
@@ -1,9 +1,11 @@
-$esfSolutionDirectory = "C:\Sources\esf\source\"
-$mongoDBServerDirectory = "C:\Program Files\MongoDB\Server\3.2\bin\"
-$mongoDBServer = $mongoDBServerDirectory + "mongod.exe"
-$dbDeploymentConfig = $esfSolutionDirectory + "DbDeploymentConfig.cfg"
-$dbPath = "C:\databases\EsFiddle"
-$logPath = "C:\databases\log\EsFiddle.log"
-New-Item -ItemType Directory -Force -Path $dbPath
-New-Item -ItemType File -Force $logPath
-& $mongoDBServer --dbpath $dbPath --logpath $logPath
+function deployDb($esfSolutionDirectory, $mongoDBServerDirectory, $dbRootDirectory, $dbName)
+{
+    $mongoDBServer = $mongoDBServerDirectory + "mongod.exe"
+    $dbPath = $dbRootDirectory + $dbName
+    $logPath = $dbRootDirectory + "log\" + $dbName + ".log"
+    New-Item -ItemType Directory -Force -Path $dbPath
+    New-Item -ItemType File -Force $logPath
+    & $mongoDBServer --dbpath $dbPath --logpath $logPath
+}
+
+deployDb "C:\Sources\esf\source\" "C:\Program Files\MongoDB\Server\3.2\bin\" "C:\databases\" "EsFiddle"
