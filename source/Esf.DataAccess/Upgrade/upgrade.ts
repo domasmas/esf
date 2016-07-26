73442c113a9ad24d0800decd03cce8187b26b352
vladotrifonov@gmail.com <vladotrifonov@gmail.com>
2016-07-26 17:03:42 +0100

    Initial configuration for MongoDB ugprade

---
+++ source/Esf.DataAccess/Upgrade/upgrade.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path='../Scripts/typings/mongodb/mongodb.d.ts' />
+
+import mongoDb = require('mongodb');
+
+module upgradeModule {
+    class Upgrader {
+        upgrade(): void {
+            console.log('upgraded');
+        }
+    }
+
+    export var upgradeInstance = new Upgrader();
+}
+
+
+
+
+upgradeModule.upgradeInstance.upgrade();
---
+++ source/Esf.DataAccess/gulpfile.js
@@ -0,0 +1,15 @@
+﻿/// <binding AfterBuild='tscompile' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('tscompile', function () {
+    var tsResult = gulp.src('Upgrade/**/*.ts')
+    .pipe(ts(tsProject));
+    tsResult.js.pipe(gulp.dest('UpgradeOutput'))
+});
