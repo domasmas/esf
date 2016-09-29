62a8527ababb83b4d63c4e27a9386c7481a566ed
vtrifonov <vladotrifonov@gmail.com>
2016-09-29 21:45:35 +0100

    #15 Improve protractor postinstall setup naming; fixed websitehomapage test

--- source/Esf.Website/App/endToEndTests/esf.WebsiteHomePage.spec.ts
+++ source/Esf.Website/App/endToEndTests/esf.WebsiteHomePage.spec.ts
@@ -1,27 +1,30 @@
 ﻿describe('Esf.Website home page', function () {
     it('should be able to navigate to it', function () {
-        browser.get('http://localhost:40082');   
-        browser.ignoreSynchronization = true;
+        browser.get('http://localhost:40082');           
+
+        browser.ignoreSynchronization = true;//workaround need to fix this
 
         expect(browser.getTitle()).toEqual('Elasticsearch Fiddler');
 
         var mappingSection: protractor.ElementFinder = element(by.cssContainingText('label', 'Mapping'));
-        expect(mappingSection.isPresent()).toBe(true);        
+        expect(mappingSection.isPresent()).toBe(true, 'mappingSection');        
 
         var querySection: protractor.ElementFinder = element(by.cssContainingText('label', 'Query'));
-        expect(querySection.isPresent()).toBe(true);    
+        expect(querySection.isPresent()).toBe(true, 'querySection');    
 
         var documentsSection: protractor.ElementFinder = element(by.cssContainingText('label', 'Documents'));
-        expect(querySection.isPresent()).toBe(true);  
+        expect(documentsSection.isPresent()).toBe(true, 'documentsSection');  
 
-        var documentsSection: protractor.ElementFinder = element(by.cssContainingText('label', 'Result'));
-        expect(querySection.isPresent()).toBe(true);     
+        var resultSection: protractor.ElementFinder = element(by.cssContainingText('label', 'Result'));
+        expect(resultSection.isPresent()).toBe(true, 'resultSection');     
 
         var runCommand: protractor.ElementFinder = element(by.cssContainingText('.btn', 'Run'));
-        expect(runCommand.isPresent()).toBe(true);  
+        expect(runCommand.isPresent()).toBe(true, 'runCommand');  
 
         var saveCommand: protractor.ElementFinder = element(by.cssContainingText('.btn', 'Save'));
-        expect(saveCommand.isPresent()).toBe(true);  
+        expect(saveCommand.isPresent()).toBe(true, 'saveCommand');  
+
+        browser.ignoreSynchronization = false;
     });
 });
 
