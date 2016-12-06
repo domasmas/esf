aad9dff64f1134af678b1c90a377a9c48bef573f
vladotrifonov@gmail.com <vladotrifonov@gmail.com>
2016-12-06 16:45:14 +0000

    #14 Add esfState.service test using real http service with spies

--- source/Esf.Website/App/esfState/esfState.service.spec.ts
+++ source/Esf.Website/App/esfState/esfState.service.spec.ts
@@ -1,5 +1,48 @@
-﻿describe('esfState.service', function () {
-    it('should pass', function () {
-        expect(true).toBe(true);
+﻿import { TestBed } from '@angular/core/testing';
+import { EsfStateService, EsfStateDto } from './esfState.service';
+import { Observable, Subject } from 'rxjs/Rx';
+import { Http, HttpModule, BaseRequestOptions, ConnectionBackend, RequestOptions } from '@angular/http';
+
+describe('esfState.service', function () {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpModule],
+            providers: [EsfStateService, Http, ConnectionBackend, BaseRequestOptions]
+        });
+    });
+    it('should work with real http service with spies', function () {
+        var response: any = {};
+        response.json = function () {
+            return {
+                id: 1,
+                prop1: "value",
+                prop2: 55
+            }
+        };
+        var subject = new Subject();
+        var http: Http = TestBed.get(Http);
+        spyOn(http, 'get').and.returnValue(subject.asObservable());
+        var esfStateService: EsfStateService = TestBed.get(EsfStateService);
+        var response2: any = {};
+        response2.json = function () {
+            return {
+                id: 2,
+                prop65656: "something",
+                fddff: 55
+            }
+        };
+
+        esfStateService.getInitialState().subscribe(state => {
+            console.log(state);
+            if (state.id === 1) {
+                expect(state).toEqual(response.json());
+            }
+            else {
+                expect(state).toEqual(response2.json());
+            }
+        });
+
+        subject.next(response);
+        subject.next(response2);
     });
 });
