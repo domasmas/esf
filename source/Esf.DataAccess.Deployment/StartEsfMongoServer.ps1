5539e93542c7101530eb0bbf048f78fe451c9771
Vladimir Trifonov <vladotrifonov@gmail.com>
2017-02-28 23:35:29 +0000

    #35 Improved MongoDB deployment - upgrade C# drivers; expose MongoDB server port in deployment.config.json; fixed EsFiddleDploymentTests -> IsDatabaseVersion3_2

--- source/Esf.DataAccess.Deployment/StartEsfMongoServer.ps1
+++ source/Esf.DataAccess.Deployment/StartEsfMongoServer.ps1
@@ -1,11 +1,11 @@
-function StartMongoDbServer($mongoDbServerDirectory, $esFiddleDbPath) {
+function StartMongoDbServer($mongoDbServerDirectory, $esFiddleDbPath, $esFiddlePort) {
 	Clear-Host
 	Write-Host -ForegroundColor Yellow "Starting Mongo DB Server for ES Fiddle"
     $mongoDbServer = $mongoDbServerDirectory + "mongod.exe"
 	if (-Not (Test-Path -Path $esFiddleDbPath)) {
 		New-Item -ItemType Directory -Force -Path "$esFiddleDbPath\DB"	
 	}
-    & $mongoDbServer --dbpath "$esFiddleDbPath\DB" --logpath "$esFiddleDbPath\Log.log"
+    & $mongoDbServer --dbpath "$esFiddleDbPath\DB" --logpath "$esFiddleDbPath\Log.log" --port "$esFiddlePort"
 }
 $dbDeploymentConfig = (Get-Content $PSScriptRoot\dbDeployment.config.json) -join "`n" | ConvertFrom-Json
-StartMongoDbServer $dbDeploymentConfig.mongoDbServerDirectory $dbDeploymentConfig.esFiddleDbPath
+StartMongoDbServer $dbDeploymentConfig.mongoDbServerDirectory $dbDeploymentConfig.esFiddleDbPath $dbDeploymentConfig.mongoDbServerPort
