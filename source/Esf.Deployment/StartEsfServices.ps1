b30fe9064ce46ce89a6f5596f3eccb357074013c
Vladimir Trifonov <vladotrifonov@gmail.com>
2017-06-08 02:30:35 +0100

    #41 Added Esf.Website and Esf.WebApi to run on StartEsfServices

--- source/Esf.Deployment/StartEsfServices.ps1
+++ source/Esf.Deployment/StartEsfServices.ps1
@@ -3,3 +3,7 @@ StartMongoDbServerInSeparateProcess
 
 Import-Module $PSScriptRoot\..\Esf.QueryRunnerDeployment\Esf.QueryRunnerDeployment.psm1
 Start-EsfQueryRunner
+
+$env:ASPNETCORE_ENVIRONMENT='Development'
+Start-Process powershell.exe -ArgumentList " -command dotnet run" -WorkingDirectory $PSScriptRoot\..\Esf.WebApi
+Start-Process powershell.exe -ArgumentList " -command dotnet run" -WorkingDirectory $PSScriptRoot\..\Esf.Website
